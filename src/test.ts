5062d6130da20c8d87d56e10278208ac6e7435a0
KillerBossOriginal <killerbossoriginal@outlook.it>
2023-12-04 19:22:51 +0100

    test

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import TNC from "./index.js";
 
-let client = new TNC({ customURL: "http://localhost:9999", token: "3Wi7CcCZyTdBWHZMhjB0BXvkScJKQq5W0CvMUYvnJUKhiuj2TpNMYd3JRbQe6MCR" });
+let client = new TNC({ customURL: "http://localhost:9999", token: "" });
 client.connect();
 // console.log(await client.credits.balance(2));
