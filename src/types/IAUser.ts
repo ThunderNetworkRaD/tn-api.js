33b50bca4e6c65d590c01630442389cf5ae07682
KillerBossOriginal <killerbossoriginal@outlook.it>
2024-07-03 16:38:33 +0200

    Aggiunto supporto per i nuovi utenti

---
+++ src/types/IAUser.ts
@@ -0,0 +1,23 @@
+import { IALanguage } from "./IALanguage"
+
+export class IAUser {
+    id: string;
+    username: string;
+    permission: string[];
+    credits: number;
+    language: IALanguage;
+
+    constructor(options: { id: string, username: string, permissions: string[], credits: number, language: string }) {
+        this.id = options.id;
+        this.username = options.username;
+        this.permission = options.permissions;
+        this.credits = options.credits;
+        switch (options.language) {
+            case "en":
+                this.language = IALanguage.EN;
+                break;
+            default:
+                this.language = IALanguage.EN;
+        }
+    }
+}
